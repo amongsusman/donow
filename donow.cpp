4d9e211c319d16380f821a84ad9a3fac1e558c0a
amongsusman <149024306+amongsusman@users.noreply.github.com>
2024-12-18 08:53:17 -0500

    Update donow.cpp

--- donow.cpp
+++ donow.cpp
@@ -9,8 +9,11 @@ Extra: None
 #include <ctime>
 using namespace std;
 int main() {
+    // sets the random seed
     srand(time(0));
+    // intializes the double pi
     double d = 3.1415926535;
+    // rounds to the 2nd decimal place by rounding on the double after it has been moved 2 digits over
     cout << round(d * 100) / 100 << endl;
     cout << (rand() % 10) + 1 << endl;
     return 0;
